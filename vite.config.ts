c2292ace4c179628f8dad102b2a9599ee4f8360a
Nelyon108 <108655277+Nelyon108@users.noreply.github.com>
2024-07-23 17:48:38 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'Home.html'),
-        about: resolve(__dirname, 'About.html'),
-        contact: resolve(__dirname, 'Contact.html')
-      }
-    }
-  }
 })
