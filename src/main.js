3744d91d744854ac13b6f8a2bc4029e92583fb5b
Nelyon108 <108655277+Nelyon108@users.noreply.github.com>
2024-07-23 17:45:09 +0000

    Tried chnaging file ext.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import React, {useRef} from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App.js'
 import './index.css'
-import Header from "./components/Header";
+import Header from "./components/Header.js";
 
 
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
 
   <React.StrictMode>
     {/* <div className='homeOverlay'></div> */}
